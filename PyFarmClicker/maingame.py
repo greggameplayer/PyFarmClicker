a7afbd4635f3ec6a3768d00bc3451d9eedefa020
Restyled.io <commits@restyled.io>
2020-03-24 17:30:54 +0000

    Restyled by black

--- PyFarmClicker/maingame.py
+++ PyFarmClicker/maingame.py
@@ -103,31 +103,31 @@ class MAINGAME:
     def onBtFinirClick(self, *_event):
         if not self.items:
             tkmessage.showwarning(
-                "Attention", "Vous n'avez pas acheté de plantes et/ou d'animaux !")
+                "Attention", "Vous n'avez pas acheté de plantes et/ou d'animaux !"
+            )
             return
-        pactolenb = re.findall(
-            r"[-+]?\d*\.?\d+|[-+]?\d+", self.pactole["text"])
-        pactolenb = float(f'{float(pactolenb[0]):.2f}')
+        pactolenb = re.findall(r"[-+]?\d*\.?\d+|[-+]?\d+", self.pactole["text"])
+        pactolenb = float(f"{float(pactolenb[0]):.2f}")
         fashionflowercolor = getFashionFlowerColor()
         diff = 0
         for item in self.items:
             if isinstance(item, Fleur) and item.getCouleur() == fashionflowercolor:
                 itemprice = item.vendre()
-                pactolenb += float(itemprice)*2
+                pactolenb += float(itemprice) * 2
                 diff = float(diff) + float(itemprice) * 2
             else:
                 itemprice = item.vendre()
                 pactolenb += itemprice
                 diff = float(diff) + float(itemprice)
-        self.pactole.configure(text=str(f'{pactolenb:.2f}') + " €")
+        self.pactole.configure(text=str(f"{pactolenb:.2f}") + " €")
         season = re.findall(r"[-+]?\d*\.?\d+|[-+]?\d+", self.saison["text"])
         season = int(season[0]) + 1
         self.saison.configure(text="Saison " + str(season))
         self.items.clear()
         if diff >= 0:
-            self.diff.configure(text="+" + str(f'{diff:.2f}') + " €")
+            self.diff.configure(text="+" + str(f"{diff:.2f}") + " €")
         else:
-            self.diff.configure(text=str(f'{diff:.2f}') + " €")
+            self.diff.configure(text=str(f"{diff:.2f}") + " €")
 
     def onComboboxChange(self, typeCombo, *_event):
         if typeCombo == "legumes":
@@ -145,7 +145,8 @@ class MAINGAME:
             child.unbind("<<ComboboxSelected>>")
             child.destroy()
         self.listeCombobox = ttk.Combobox(
-            self.centerbottomframe, values=getLegumesNames(), state="readonly")
+            self.centerbottomframe, values=getLegumesNames(), state="readonly"
+        )
         self.listeCombobox.pack()
         self.listeCombobox.current(0)
         self.PriceLabel = Label(self.centerbottomframe, text="0.5 €")
@@ -157,8 +158,9 @@ class MAINGAME:
             width=20,
         )
         self.buyBt.pack(pady="10")
-        self.listeCombobox.bind("<<ComboboxSelected>>", partial(
-            self.onComboboxChange, "legumes"))
+        self.listeCombobox.bind(
+            "<<ComboboxSelected>>", partial(self.onComboboxChange, "legumes")
+        )
         self.buyBt.bind("<Button-1>", partial(self.onBtBuyClick, "legumes"))
 
     def onBtFleursClick(self, *_event):
@@ -167,7 +169,8 @@ class MAINGAME:
             child.unbind("<<ComboboxSelected>>")
             child.destroy()
         self.listeCombobox = ttk.Combobox(
-            self.centerbottomframe, values=getFlowersNames(), state="readonly")
+            self.centerbottomframe, values=getFlowersNames(), state="readonly"
+        )
         self.listeCombobox.pack()
         self.listeCombobox.current(0)
         self.PriceLabel = Label(self.centerbottomframe, text="2.0 €")
@@ -179,8 +182,9 @@ class MAINGAME:
             width=20,
         )
         self.buyBt.pack(pady="10")
-        self.listeCombobox.bind("<<ComboboxSelected>>",
-                                partial(self.onComboboxChange, "fleurs"))
+        self.listeCombobox.bind(
+            "<<ComboboxSelected>>", partial(self.onComboboxChange, "fleurs")
+        )
         self.buyBt.bind("<Button-1>", partial(self.onBtBuyClick, "fleurs"))
 
     def onBtBuyClick(self, typeCombo, *_event):
@@ -189,26 +193,30 @@ class MAINGAME:
                 if legume.getNom() == self.listeCombobox.get():
                     self.items.append(legume)
                     pactolenumber = re.findall(
-                        r"[-+]?\d*\.?\d+|[-+]?\d+", self.pactole["text"])
+                        r"[-+]?\d*\.?\d+|[-+]?\d+", self.pactole["text"]
+                    )
                     pactolenumber[0] = float(pactolenumber[0])
-                    pactolenumber = float(f'{pactolenumber[0]:.2f}')
+                    pactolenumber = float(f"{pactolenumber[0]:.2f}")
                     if pactolenumber - legume.getPrixAchat() < 0:
                         tkmessage.showwarning(
-                            "Attention", "Vous n'avez plus assez de flouz !")
+                            "Attention", "Vous n'avez plus assez de flouz !"
+                        )
                         break
                     pactolenumber -= legume.getPrixAchat()
-                    self.pactole["text"] = str(f'{pactolenumber:.2f}') + " €"
+                    self.pactole["text"] = str(f"{pactolenumber:.2f}") + " €"
         elif typeCombo == "fleurs":
             for fleur in Fleurs:
                 if fleur.getNom() == self.listeCombobox.get():
                     self.items.append(fleur)
                     pactolenumber = re.findall(
-                        r"[-+]?\d*\.?\d+|[-+]?\d+", self.pactole["text"])
+                        r"[-+]?\d*\.?\d+|[-+]?\d+", self.pactole["text"]
+                    )
                     pactolenumber[0] = float(pactolenumber[0])
-                    pactolenumber = float(f'{pactolenumber[0]:.2f}')
+                    pactolenumber = float(f"{pactolenumber[0]:.2f}")
                     if pactolenumber - fleur.getPrixAchat() < 0:
                         tkmessage.showwarning(
-                            "Attention", "Vous n'avez plus assez de flouz !")
+                            "Attention", "Vous n'avez plus assez de flouz !"
+                        )
                         break
                     pactolenumber -= fleur.getPrixAchat()
-                    self.pactole["text"] = str(f'{pactolenumber:.2f}') + " €"
+                    self.pactole["text"] = str(f"{pactolenumber:.2f}") + " €"
